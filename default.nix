10683b421c7f8a0018c6cf94d41001e7b95f5c07
Daniel Baker <daniel.n.baker@gmail.com>
2023-09-25 10:14:09 -0700

    add documentation.

--- default.nix
+++ default.nix
@@ -28,67 +28,159 @@
   parseIpAddr = ipAddr:
     builtins.map lib.toInt (builtins.match "([0-9]+)\\.([0-9]+)\\.([0-9]+)\\.([0-9]+)" ipAddr);
 
+  /*
+  Given a CIDR, return the IP Address.
+
+  Type: cidrToIpAddress :: str -> [ int ]
+
+  Examples:
+    cidrToIpAddress "192.168.70.9/15"
+    => [ 192 168 70 9 ]
+  */
   cidrToIpAddress = cidr: let
     splitParts = lib.splitString "/" cidr;
   in
     parseIpAddr (lib.elemAt splitParts 0);
 
+  /*
+  Given a CIDR, return the bitmask.
+
+  Type: cidrToBitMask :: str -> int
+
+  Examples:
+    cidrToIpAddress "192.168.70.9/15"
+    => 15
+  */
   cidrToBitMask = cidr: let
     splitParts = lib.splitString "/" cidr;
   in
     lib.toInt (lib.elemAt splitParts 1);
 
+  /*
+  Given a CIDR, return the associated subnet mask.
+
+  Type: cidrToSubnetMask :: str -> [ int ]
+
+  Examples:
+    cidrToSubnetMask "192.168.70.9/15"
+    => [ 255 254 0 0 ]
+  */
   cidrToSubnetMask = cidr:
     bitMaskToSubnetMask (cidrToBitMask cidr);
 
+  /*
+  Given a CIDR, return the associated network ID.
+
+  Type: cidrToNetworkId :: str -> [ int ]
+
+  Examples:
+    cidrToNetworkId "192.168.70.9/15"
+    => [ 192 168 0 0 ]
+  */
   cidrToNetworkId = cidr: let
     ip = cidrToIpAddress cidr;
     subnetMask = cidrToSubnetMask cidr;
   in
     lib.zipListsWith lib.bitAnd ip subnetMask;
 
-  cidrToLowerIp = cidr: let
-    networkId = cidrToNetworkId cidr;
-  in
-    getLowerIp networkId;
+  /*
+  Given a CIDR, return the associated first usable IP address.
 
-  getLowerIp = ip: let
-    lastOctet = lib.last ip;
-    firstThree = lib.init ip;
+  Type: cidrToFirstUsableIp :: str -> [ int ]
+
+  Examples:
+    cidrToFirstUsableIp "192.168.70.9/15"
+    => [ 192 168 0 0 ]
+  */
+  cidrToFirstUsableIp = cidr: let
+    networkId = cidrToNetworkId cidr;
+    lastOctet = lib.last networkId;
+    firstThree = lib.init networkId;
   in
     firstThree ++ [(lastOctet + 1)];
 
+  /*
+  Given a CIDR, return the associated broadcast address.
+
+  Type: cidrToBroadcastAddress :: str -> [ int ]
+
+  Examples:
+    cidrToBroadcastAddress "192.168.70.9/15"
+    => [ 192 169 255 255 ]
+  */
   cidrToBroadcastAddress = cidr: let
-    bitMask = cidrToBitMask cidr;
+    subnetMask = cidrToSubnetMask cidr;
     networkId = cidrToNetworkId cidr;
   in
-    getBroadcastAddress networkId (bitMaskToSubnetMask bitMask);
+    getBroadcastAddress networkId subnetMask;
 
-  getBroadcastAddress = networkId: bitMask:
-    lib.zipListsWith (b: m: 255 - m + b) networkId bitMask;
+  /*
+  Given a network ID and subnet mask, return the associated broadcast address.
 
-  cidrToUpperIp = cidr: let
+  Type: getBroadcastAddress :: [ int ] -> [ int ] -> [ int ]
+
+  Examples:
+    getBroadcastAddress [ 192 168 0 0 ] [ 255 254 0 0 ]
+    => [ 192 169 255 255 ]
+  */
+  getBroadcastAddress = networkId: subnetMask:
+    lib.zipListsWith (nid: mask: 255 - mask + nid) networkId subnetMask;
+
+  /*
+  Given a CIDR, return the associated last usable IP address.
+
+  Type: cidrToLastUsableIp :: str -> [ int ]
+
+  Examples:
+    cidrToLastsableIp "192.168.70.9/15"
+    => [ 192 169 255 254 ]
+  */
+  cidrToLastUsableIp = cidr: let
     broadcast = cidrToBroadcastAddress cidr;
     lastOctet = lib.last broadcast;
     firstThree = lib.init broadcast;
   in
     firstThree ++ [(lastOctet - 1)];
 
+  /*
+  Given a CIDR, return an attribute set of:
+    the IP Address,
+    the bit mask,
+    the first usable IP address,
+    the last usable IP address,
+    the network ID,
+    the subnet mask,
+    the broadcast address.
+
+  Type: getNetworkProperties :: str -> attrset
+
+  Examples:
+    getNetworkProperties "192.168.70.9/15"
+    => {
+      bitMask = 15;
+      broadcast = [ 192 169 255 255 ];
+      firstUsableIp = [ 192 168 0 1 ];
+      ipAddress = [ 192 168 70 9 ];
+      lastUsableIp = [ 192 169 255 254 ];
+      networkId = [ 192 168 0 0 ];
+      subnetMask = [ 255 254 0 0 ];
+    }
+  */
   getNetworkProperties = cidr: let
-    ip = cidrToIpAddress cidr;
+    ipAddress = cidrToIpAddress cidr;
     bitMask = cidrToBitMask cidr;
-    lower = cidrToLowerIp cidr;
-    upper = cidrToUpperIp cidr;
+    firstUsableIp = cidrToFirstUsableIp cidr;
+    lastUsableIp = cidrToLastUsableIp cidr;
     networkId = cidrToNetworkId cidr;
     subnetMask = cidrToSubnetMask cidr;
     broadcast = cidrToBroadcastAddress cidr;
-  in {inherit ip bitMask lower upper networkId subnetMask broadcast;};
+  in {inherit ipAddress bitMask firstUsableIp lastUsableIp networkId subnetMask broadcast;};
 in {
   inherit
     cidrToIpAddress
     cidrToBitMask
-    cidrToLowerIp
-    cidrToUpperIp
+    cidrToFirstUsableIp
+    cidrToLastUsableIp
     cidrToNetworkId
     cidrToSubnetMask
     cidrToBroadcastAddress
